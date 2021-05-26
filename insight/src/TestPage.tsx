00fb4c623c8c33fcdbd36306658c2d1115930a57
Alex Withnell <alexanderwithnell@gmail.com>
2021-05-26 00:14:34 -0700

    State Pratice

---
+++ insight/src/TestPage.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import {Button} from '@material-ui/core'
+
+
+function 
+
+function TestPage(){
+    const [checked, setChecked] = useState(true);
+    let result: any;
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{ setChecked(event.target.checked) }
+    return (
+        <div>
+            <Button id='CCButton' variant='contained' onClick={() => {}} color='primary'>Copy To Clipboard</Button>
+        </div>
+    )
+}
+
+
+export default TestPage;
