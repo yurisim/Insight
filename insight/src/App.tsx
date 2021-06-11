65c17f361170ded47e4d0509776e07c28e3c4067
eric.cater <eric.cater@us.af.mil>
2021-06-10 19:45:59 -0500

    Added visual indicator, tab switching, searching

--- insight/src/App.tsx
+++ insight/src/App.tsx
@@ -3,6 +3,9 @@ import PersonGrid from './components/PersonGrid'
 import StatusTabs from './components/StatusTabs'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import {columns} from './junkdata';
+import { Component } from "react";
+import axios from "axios";
+import { GridRowsProp } from "@material-ui/x-grid";
 
 export interface CreatePersonDTO {
   name: string;
@@ -15,14 +18,72 @@ export interface CreatePersonDTO {
   comments: string;
 }
 
-function App(){
-  return (
-    <div className="App">
-      <CssBaseline/>
-      <StatusTabs />
-      <PersonGrid daColumns={columns}/>
-    </div>
-  );
+/**
+ * Represents the status tabs' possible values
+ */
+export enum enumStatusView { OVERDUE = "OVERDUE", SAFE = "SAFE", UPCOMING = "UPCOMING", ALL = "ALL", }
+
+/**
+ * App State
+ */
+interface IAppState {
+  selectedStatus: enumStatusView;
+  persons: GridRowsProp;
+}
+
+class App extends Component<{}, IAppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //This sets default status tab
+      selectedStatus: enumStatusView.ALL,
+      persons: [],
+    };
+    this.getData("http://localhost:5000/person/getAll");
+  }
+
+  /**
+   * Async data retrival from backend
+   * @param inputUrl
+   */
+  getData(inputUrl: string) {
+    axios({
+      method: "get",
+      url: inputUrl,
+    }).then((res) => {
+      let persons = res.data;
+
+      persons.forEach((element) => {
+        element.id = element._id;
+      });
+      this.setState({ persons: persons });
+    });
+  }
+
+  /**
+   * called when selected tab changes, stores value in state
+   * @param status
+   */
+  changeStatusTab(status: enumStatusView) {
+    this.setState({ selectedStatus: status });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CssBaseline />
+        <StatusTabs
+          selectedStatus={this.state.selectedStatus}
+          changeStatusTab={(selectedStatus) => this.changeStatusTab(selectedStatus) }
+        />
+        <PersonGrid
+          columns={columns}
+          persons={this.state.persons}
+          selectedStatus={this.state.selectedStatus}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- insight/src/junkdata.ts
+++ insight/src/junkdata.ts
@@ -1,6 +1,6 @@
-import { GridColDef } from '@material-ui/data-grid';
+import { GridColumns } from '@material-ui/x-grid'
 
-export const columns: GridColDef[] = [
+export const columns: GridColumns = [
     { field: 'id', headerName: 'ID', hide: true },
     { field: 'dodid', headerName: 'DoDID', width: 125 },
     { field: 'lastName', headerName: 'Last Name', width: 150 },
@@ -11,67 +11,4 @@ export const columns: GridColDef[] = [
     { field: 'status', headerName: 'Status', width:120 },
     { field: 'dueDate', headerName: 'Due Date', width:130 },
     { field: 'comments', headerName: 'Comments', width: 200, editable: true },
-  ];
-
-export const rows = [
-    {
-        id: 1,
-        lastName: "Castle",
-        firstName: "Grif",
-        dodid: 1234567890,
-        afscid: 4,
-        workCenter: "DOUP",
-        timeOnStation: "10/06/2018",
-        status: "Safe",
-        dueDate: "10/06/2019",
-        comments: "awesome"
-    },
-    {
-        id: 2,
-        lastName: "Sim",
-        firstName: "Yuri",
-        dodid: 1234567891,
-        afscid: 4,
-        workCenter: "DOUP",
-        timeOnStation: "07/06/2020",
-        status: "Upcoming",
-        dueDate: "07/06/2021",
-        comments: "eh"
-    },
-    {
-        id: 3,
-        lastName: "Myers",
-        firstName: "Ryan",
-        dodid: 1234567892,
-        afscid: 3,
-        workCenter: "DOK",
-        timeOnStation: "11/06/2018",
-        status: "Safe",
-        dueDate: "10/06/2019",
-        comments: "awesome-ish"
-    },
-    {
-        id: 4,
-        lastName: "Sullivan",
-        firstName: "Nathan",
-        dodid: 1234567893,
-        afscid: 2,
-        workCenter: "DOM",
-        timeOnStation: "03/06/2019",
-        status: "Safe",
-        dueDate: "03/06/2020",
-        comments: "awesome"
-    },
-    {
-        id: 5,
-        lastName: "Withnell",
-        firstName: "Alexander",
-        dodid: 1234567894,
-        afscid: 4,
-        workCenter: "DOUP",
-        timeOnStation: "10/06/2018",
-        status: "Overdue",
-        dueDate: "10/06/2019",
-        comments: "sleepy"
-    }
   ];
