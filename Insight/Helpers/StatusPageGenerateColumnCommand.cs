27346188ce2616911484f6c8ce0e77e85e860d02
Yuri Sim <edaraks@gmail.com>
2021-08-24 13:46:04 -0500

    Comments on StatusPage Column Reformatting

--- Insight/Helpers/StatusPageGenerateColumnCommand.cs
+++ Insight/Helpers/StatusPageGenerateColumnCommand.cs
@@ -16,14 +16,24 @@ namespace Insight.Helpers
 			//this.Id = CommandId.DataBindingComplete;
 		}
 
+		/// <summary>
+		/// This is a delegate command that checks if the column should be rendered. By default it'll render every column.
+		/// </summary>
+		/// <param name="parameter"></param>
+		/// <returns></returns>
 		public override bool CanExecute(object parameter)
 		{
-			var context = parameter as GenerateColumnContext;
+			// var context = parameter as GenerateColumnContext;
 			// put your custom logic here
-			
+
+			//
 			return true;
 		}
 
+		/// <summary>
+		/// Executes the following code on each column where CanExecute is true.
+		/// </summary>
+		/// <param name="parameter"></param>
 		public override void Execute(object parameter)
 		{
 			var context = parameter as GenerateColumnContext;
