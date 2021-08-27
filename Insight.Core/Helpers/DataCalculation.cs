cfeaf32b9788d6d0b14c71f6ffbbc95b51c782fd
eric.cater <eric.cater@us.af.mil>
2021-08-27 13:49:58 -0500

    Reworked DataCalculation

--- Insight.Core/Helpers/DataCalculation.cs
+++ Insight.Core/Helpers/DataCalculation.cs
@@ -1,103 +1,48 @@
 ﻿using Insight.Core.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 namespace Insight.Core.Helpers
 {
-   public static class DataCalculation
-   {
+	public static class DataCalculation
+	{
 		/// <summary>
-		/// Calculates percentage of people ready with medical
+		/// Returns a percentage of people full ready in all catagories. If no persons in passed list, returns all 100%
 		/// </summary>
-		/// <param name="data">the group you want to check</param>
+		/// <param name="persons"></param>
 		/// <returns></returns>
-        public static string GetMedical(List<Person> data)
-        {
-            string medicalPercentageOutput = "Unknown";
-            if (data.Count != 0)
-            {
-                decimal medicalPercentage = 0;
-                foreach (var item in data)
-                {
-                    if (item.Medical?.OverallStatus == Status.Current || item.Medical?.OverallStatus == Status.Upcoming)
-                    {
-                        medicalPercentage++;
-                    }
-                }
-                medicalPercentage /= data.Count;
-                medicalPercentageOutput = string.Format("{0:P}", medicalPercentage);
-            }
-            return medicalPercentageOutput;
-        }
-		/// <summary>
-		/// Calculates percentage of people ready with personnel items
-		/// </summary>
-		/// <param name="data">the group you want to check</param>
-		/// <returns></returns>
-		public static string GetPersonnel(List<Person> data)
-        {
-            string personnelPercentageOutput = "Unknown";
-            if (data.Count != 0)
-            {
-                decimal personnelPercentage = 0;
-                foreach (var item in data)
-                {
-                    if (item.Personnel?.OverallStatus == Status.Current || item.Personnel?.OverallStatus == Status.Upcoming)
-                    {
-                        personnelPercentage++;
-                    }
-                }
-                personnelPercentage /= data.Count;
-                personnelPercentageOutput = string.Format("{0:P}", personnelPercentage);
-            }
-            return personnelPercentageOutput;
-        }
-		/// <summary>
-		/// Calculates percentage of people ready with training
-		/// </summary>
-		/// <param name="data">the group you want to check</param>
-		/// <returns></returns>
-		public static string GetTraining(List<Person> data)
-        {
-            string trainingPercentageOutput = "Unknown";
-            if (data.Count != 0)
-            {
-                decimal trainingPercentage = 0;
-                foreach (var item in data)
-                {
-                    if (item.Training?.OverallStatus == Status.Current || item.Training?.OverallStatus == Status.Upcoming)
-                    {
-                        trainingPercentage++;
-                    }
-                }
-                trainingPercentage /= data.Count;
-                trainingPercentageOutput = string.Format("{0:P}", trainingPercentage);
-            }
-            return trainingPercentageOutput;
-        }
-		/// <summary>
-		/// Calculates percentage of people ready with training, personnel items, and medical
-		/// </summary>
-		/// <param name="data">the group you want to check</param>
-		/// <returns></returns>
-		public static string GetOverall(List<Person> data)
+		public static decimal GetReadinessPercentage(List<Person> persons)
 		{
-			string overallPercentageOutput = "Unknown";
-			if (data.Count != 0)
-			{
-				decimal overallPercentage = 0;
-				foreach (var item in data)
-				{
-					if ((item.Training?.OverallStatus == Status.Current || item.Training?.OverallStatus == Status.Upcoming) && (item.Personnel?.OverallStatus == Status.Current || item.Personnel?.OverallStatus == Status.Upcoming) &&
-						(item.Medical?.OverallStatus == Status.Current || item.Medical?.OverallStatus == Status.Upcoming))
-					{
-						overallPercentage++;
-					}
-				}
-				overallPercentage /= data.Count;
-				overallPercentageOutput = string.Format("{0:P}", overallPercentage);
-			}
-			return overallPercentageOutput;
+			if (persons.Count == 0) return 1;
+
+			var validStatus = new List<Status>() { Status.Current, Status.Upcoming };
+
+			decimal numValid = persons.Count(p =>
+				validStatus.Contains(p.Medical.OverallStatus) &&
+				validStatus.Contains(p.Personnel.OverallStatus) &&
+				validStatus.Contains(p.Training.OverallStatus));
+			return numValid / persons.Count;
 		}
-    }
+
+		/// <summary>
+		/// Returns a typle containing the overall status of Medical, Personnel, Training. If no persons in passed list, returns all 100%
+		/// </summary>
+		/// <param name="persons"></param>
+		/// <returns></returns>
+		public static (decimal, decimal, decimal) GetReadinessPerctageByCategory(List<Person> persons)
+		{
+			if (persons.Count == 0) return (1m, 1m, 1m);
+
+			var validStatus = new List<Status>() { Status.Current, Status.Upcoming };
+
+			decimal numValidMedical = persons.Count(p => validStatus.Contains(p.Medical.OverallStatus));
+			decimal numValidPersonnel = persons.Count(p => validStatus.Contains(p.Personnel.OverallStatus));
+			decimal numValidTraining = persons.Count(p => validStatus.Contains(p.Training.OverallStatus));
+
+			//return in tuple
+			return (numValidMedical / persons.Count, numValidPersonnel / persons.Count, numValidTraining / persons.Count);
+		}
+	}
 }
