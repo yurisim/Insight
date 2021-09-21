9ec301710457bb940017d7647f4ffa2fbea48c68
Withnell <alexander.withnell@us.af.mil>
2021-09-21 09:49:11 -0500

    Moved tests and fixed spacing

--- Insight.Tests.MSTest/Tests.cs
+++ Insight.Tests.MSTest/Tests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Insight.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Insight.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
 
 namespace Insight.Tests.MSTest
@@ -46,39 +45,6 @@ namespace Insight.Tests.MSTest
 			Assert.IsNotNull(vm);
 		}
 
-		[UITestMethod]
-		public void TestPercentageVeryHighStyleSelector()
-		{
-			PercentageColor percentageColor = new PercentageColor();
-			Assert.AreEqual(percentageColor.VeryHighPercent, percentageColor.GetStyle(95));
-		}
-
-		[UITestMethod]
-		public void TestPercentageHighStyleSelector()
-		{
-			PercentageColor percentageColor = new PercentageColor();
-			Assert.AreEqual(percentageColor.HighPercent, percentageColor.GetStyle(85));
-		}
-
-		[UITestMethod]
-		public void TestPercentageMidStyleSelector()
-		{
-			PercentageColor percentageColor = new PercentageColor();
-			Assert.AreEqual(percentageColor.MediumPercent, percentageColor.GetStyle(73));
-		}
-
-		[UITestMethod]
-		public void TestPercentageLowStyleSelector()
-		{
-			PercentageColor percentageColor = new PercentageColor();
-			Assert.AreEqual(percentageColor.LowPercent, percentageColor.GetStyle(64));
-		}
-
-		[UITestMethod]
-		public void TestPercentageVeryLowStyleSelector()
-		{
-			PercentageColor percentageColor = new PercentageColor();
-			Assert.AreEqual(percentageColor.VeryLowPercent, percentageColor.GetStyle(32));
-		}
+		
 	}
 }
